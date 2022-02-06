3a3e7532bf6439c33494fefec96da4f40b72c253
Yoon Joo Lee <yoonjoolee@INV00043.fritz.box>
2022-02-06 23:44:58 +0100

    Horizontal Light list done

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+import 'dart:ffi';
+
+import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -13,131 +16,124 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Managing state',
+      title: 'Switch List',
       home: Scaffold(
-          appBar: AppBar(
-            title: const Text('Managing state'),
-          ),
-          /*body: const Center(
-          child: TapboxA(),
+        appBar: AppBar(
+          title: const Text('Switch List'),
         ),
-        */
-          body: ListView(
-              children : [
-
-                const TapboxA(),
-                const ParentWidget(),
-
-              ]
-          )
-
+        body: const Center(
+          child: SwitchList(),
+        ),
       ),
     );
   }
 }
 
-//------------------------- TapboxA ----------------------------------
-
-class TapboxA extends StatefulWidget {
-  const TapboxA({Key? key}) : super(key: key);
+class SwitchList extends StatefulWidget {
+  const SwitchList({Key? key}) : super(key: key);
 
   @override
-  _TapboxAState createState() => _TapboxAState();
+  State<StatefulWidget> createState() => _SwitchListState();
 }
 
-class _TapboxAState extends State<TapboxA> {
-  bool _active = false;
+class _SwitchListState extends State<SwitchList> {
+  List<SwitchItemModel> switchItems = [
+    SwitchItemModel('Living Room', false, Icons.living_outlined),
+    SwitchItemModel('Kitchen', false, Icons.kitchen_outlined),
+    SwitchItemModel('Bathroom', false, Icons.bathroom_outlined),
+    SwitchItemModel('Bedroom', false, Icons.bed_outlined),
+    SwitchItemModel('Office', false, Icons.laptop_mac_outlined),
+    SwitchItemModel('Dining Room', false, Icons.local_dining_outlined),
+  ];
 
-  void _handleTap() {
+  void _handleTap(int index, bool isOn) {
     setState(() {
-      _active = !_active;
+      isOn = !isOn;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: _handleTap,
-      child: Container(
-        child: Center(
-          child: Icon(
-            Icons.lightbulb_outline,
-            color: _active ? Colors.yellow.shade600 : Colors.black,
-            size: 60
-          ),
+    return Scaffold(
+      body: Container(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            SizedBox(
+              height: 200,
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: switchItems.length,
+                itemBuilder: (context, index) {
+                  final switchItem = switchItems[index];
+                  return GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        // Toggle light when tapped.
+                        switchItem.isOn = !switchItem.isOn;
+                      });
+                    },
+                    child: Container(
+                      child: Center(
+                        child: Icon(switchItem.icon,
+                            color: switchItem.isOn
+                                ? Colors.yellow.shade600
+                                : Colors.black,
+                            size: 60),
+                      ),
+                      height: 100,
+                      width: 100,
+                    ),
+                  );
+                },
+              ),
+            ),
+          ],
         ),
-        width: 200.0,
-        height: 200.0,
-        /*decoration: BoxDecoration(
-          color: _active ? Colors.lightGreen[700] : Colors.grey[600],
-        ),*/
-      ),
-    );
-  }
-}
-//------------------------ ParentWidget --------------------------------
-
-class ParentWidget extends StatefulWidget {
-  const ParentWidget({Key? key}) : super(key: key);
-
-  @override
-  _ParentWidgetState createState() => _ParentWidgetState();
-}
-
-class _ParentWidgetState extends State<ParentWidget> {
-  bool _active = false;
-
-  void _handleTapboxChanged(bool newValue) {
-    setState(() {
-      _active = newValue;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      child: TapboxB(
-        active: _active,
-        onChanged: _handleTapboxChanged,
       ),
     );
   }
 }
 
-//------------------------- TapboxB ----------------------------------
+Widget _buildHorizontalList() {
+  return SizedBox(
+    height: 200,
+    width: 200,
+    child: ListView(
+      shrinkWrap: true,
+      scrollDirection: Axis.horizontal,
+      children: [
+        _tile('Living Room', false, Icons.living_outlined),
+        _tile('Kitchen', false, Icons.kitchen_outlined),
+        _tile('Bathroom', false, Icons.bathroom_outlined),
+        _tile('Bedroom', false, Icons.bed_outlined),
+        _tile('Office', false, Icons.laptop_mac_outlined),
+        _tile('Dining Room', false, Icons.local_dining_outlined),
+      ],
+    ),
+  );
+}
 
-class TapboxB extends StatelessWidget {
-  const TapboxB({
-    Key? key,
-    this.active = false,
-    required this.onChanged,
-  }) : super(key: key);
+ListTile _tile(String name, bool isOn, IconData icon) {
+  return ListTile(
+/*    title: Text(name,
+        style: const TextStyle(
+          fontWeight: FontWeight.w500,
+          fontSize: 20,
+        )),*/
+    selected: isOn,
+    leading: Icon(
+      icon,
+      color: isOn ? Colors.yellow.shade600 : Colors.black,
+      size: 60,
+    ),
+  );
+}
 
-  final bool active;
-  final ValueChanged<bool> onChanged;
+class SwitchItemModel {
+  SwitchItemModel(this.name, this.isOn, this.icon);
 
-  void _handleTap() {
-    onChanged(!active);
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: _handleTap,
-      child: Container(
-        child: Center(
-          child: Icon(
-              Icons.lightbulb_outline,
-              color: active ? Colors.yellow.shade600 : Colors.black,
-              size: 60
-          ),
-        ),
-        width: 200.0,
-        height: 200.0,
-       /* decoration: BoxDecoration(
-          color: active ? Colors.lightBlue[700] : Colors.grey[600],
-        ),*/
-      ),
-    );
-  }
+  String name;
+  bool isOn;
+  IconData icon;
 }
